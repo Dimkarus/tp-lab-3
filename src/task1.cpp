3cfbc2607d2bddb3d419fda53d6aba1fd53e8a4e
DeniDo <43599289+ltybcyfhbr@users.noreply.github.com>
2018-11-30 22:42:26 +0300

    ааааааааааааааааааааааааааааааа

--- src/task1.cpp
+++ src/task1.cpp
@@ -5,24 +5,24 @@
 double calcDelta() 
 {
 	Circle circle;
-	circle.setRadius(r);
+	circle.setRadius(6378.1);
 	circle.setFerence(circle.getFerence() + 0.001);
 	
-	return (circle.getRadius() - r) * 1000; 
+	return (circle.getRadius() - 6378.1) * 1000; 
 }
 
 double calcCost()
 {
 	Circle circle;
-	circle.setRadius(r + 1.0);
+	circle.setRadius(3.0 + 1.0);
 
 	double tmp = circle.getFerence() * 2000; 
 
 	double s1, s2;
 
-	circle.setRadius(r);
+	circle.setRadius(3.0);
 	s1 = circle.getArea();
-	circle.setRadius(r + 1.0);
+	circle.setRadius(3.0 + 1.0);
 	s2 = circle.getArea();
 
 	tmp += (s2 - s1) * 1000; 
