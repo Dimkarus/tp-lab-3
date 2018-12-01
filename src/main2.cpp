5d8976f934a5430c21851475ba652f8e8daafa2a
Artem Aroslankin <43597601+orrrrtem@users.noreply.github.com>
2018-12-02 01:17:49 +0300

    Add files via upload

---
+++ src/main2.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Артем Аросланкин on 02/12/2018.
+//
+
+#include "task1.h"
+#include "DateTime.h"
+#include "hashT.h"
+#include "hashFun.h"
+#include <ctime>
+#include <time.h>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    cout << calcCost() << "rub" << endl;
+    return 0;
+}
---
+++ src/main3.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Артем Аросланкин on 02/12/2018.
+//
+
+#include "Circle.h"
+#include <iostream>
+#include "task1.h"
+#include "DateTime.h"
+#include "hashT.h"
+#include "hashFun.h"
+#include <ctime>
+#include <time.h>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    time_t v = time(NULL);
+    cout << v << endl;
+    tm * t = localtime(&v);
+
+
+    DateTime dt1;
+    cout << " Tomorrow is    " << dt1.getTomorrow() << endl;
+
+    DateTime dt2(2, 11, 2018);
+    string expect2 = "01 november 2018, friday";
+    cout << expect2 << "    " << dt2.getYesterday() << endl;
+
+    DateTime dt3(1, 11, 2018);
+    string expect3 = "01 november 2018, thursday";
+    cout << expect3 << " " << dt3.getToday() << endl;
+
+    DateTime dt4(20, 11, 2018);
+    DateTime dt5(24, 11, 2018);
+    unsigned long int d = 4;
+    cout << d << " =  " << dt4.getDifference(dt5) << endl;
+
+    return 0;
+}
