563ca9f70b59aa3e900d87670f4576c12f972ba8
zile <elizabet3000@mail.ru>
2018-12-02 14:39:55 +0300

    Task1

---
+++ src/main3.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Admin on 02.12.2018.
+//
+
---
+++ src/task1.cpp
@@ -0,0 +1,21 @@
+#include "task1.h"
+#include "Circle.cpp"
+
+double calcDelta(){
+
+    double rE = 6378.1 * 1000;
+    auto *Earth = new Circle(rE);
+    Earth -> setFerence(Earth -> getFerence() + 1);
+
+    return Earth -> getRadius() - rE;
+
+}
+
+double calcCost(){
+
+    double pr1 = 1000, pr2 = 2000;
+    auto *Circle1 = new Circle(4.), *Circle2 = new Circle(3.);
+
+    return (Circle1 -> getArea() - Circle2 -> getArea()) * pr1 + Circle1 -> getFerence() * pr2;
+
+}
