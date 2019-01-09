83defc92dceee8645a5bbbb97d6a69a0f180011e
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2019-01-09 12:04:23 +0300

    4

--- include/Circle.h
+++ include/Circle.h
@@ -14,10 +14,10 @@ private:
 
 public:
 	Circle();
-	Circle(double radius);
-	void setRadius(double radius);
-	void setFerence(double ference);
-	void setArea(double area);
+	Circle(double rad);
+	void setRadius(double rad);
+	void setFerence(double fer);
+	void setArea(double ar);
 	const double getRadius() const;
 	const double getFerence() const;
 	const double getArea() const;
