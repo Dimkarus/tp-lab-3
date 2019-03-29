88cae94bffee7b13c06724ba511467bc8ef6b762
Dmitry Vostrykov <38708105+Dimkarus@users.noreply.github.com>
2019-03-29 03:55:29 +0300

    Update DataTime.h

--- include/DataTime.h
+++ include/DataTime.h
@@ -4,19 +4,19 @@
 #include <string>
 #include <cmath>
 
-class Datetime
+class DateTime
 {
 private:
 	time_t time_now;
 	std::string date;
 public:
-	Datetime();
-	Datetime(unsigned int day, unsigned int month, unsigned int year);
-	Datetime(const Datetime &time);
+	DateTime();
+	DateTime(unsigned int day, unsigned int month, unsigned int year);
+	DateTime(const DateTime &time);
 	std::string getToday();
 	std::string getTomorrow();
 	std::string getYesterday();
 	std::string getFuture(unsigned int N);
 	std::string getPast(unsigned int N);
-	unsigned int calcDifference(Datetime & Datetime);
+	unsigned int calcDifference(DateTime & DateTime);
 };
